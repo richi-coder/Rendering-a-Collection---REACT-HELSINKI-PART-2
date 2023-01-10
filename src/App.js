8ae8334a2dace71e926208122f01c709ff6b029a
richi-coder <113008174+richi-coder@users.noreply.github.com>
2023-01-10 14:48:20 -0400

    Adding useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./style.css";
 import Note from "./components/Note"
+import { useState } from 'react'
 
-const App = ({notes}) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note =>
+        {props.notes.map(note =>
         <Note key={note.id} note={note} />
         )}
       </ul>
