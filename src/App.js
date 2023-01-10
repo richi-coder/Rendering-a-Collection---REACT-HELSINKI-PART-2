c2ac529e2d9e99390b948459fb4f5d7b23c59e4c
richi-coder <113008174+richi-coder@users.noreply.github.com>
2023-01-10 15:24:49 -0400

    Adjusting addNote Function for adding new notes to state

--- src/App.js
+++ src/App.js
@@ -10,8 +10,16 @@ const App = (props) => {
   ) 
 
   const addNote = (event) => {
-    event.preventDefault();
-    console.log("button clicked", event.target)
+    event.preventDefault()
+    const noteObject = {
+    content: newNote,
+    date: new Date().toISOString(),
+    important: Math.random() < 0.5,
+    id: notes.length + 1,
+  }
+
+  setNotes(notes.concat(noteObject))
+  setNewNote('')
   }
 
   const handleNoteChange = (event) => {
