f368d8bfef3c336760e812c7fd3df0b4eae7e5e0
richi-coder <113008174+richi-coder@users.noreply.github.com>
2023-01-10 12:36:29 -0400

    Adding keys for each element in the map

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ const App = (props) => {
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li>{note.content}</li>)}
+        {notes.map(note =>
+        <li key={note.id}>
+          {note.content}
+        </li>)}
       </ul>
     </div>
   )
