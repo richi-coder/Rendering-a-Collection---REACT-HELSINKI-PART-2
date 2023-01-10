faf3a8cfe34ad04ceb71ab15f13ce9879a637e26
richi-coder <113008174+richi-coder@users.noreply.github.com>
2023-01-10 12:44:54 -0400

    Creating a component for note

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./style.css";
 
+const Note = ({note}) => {
+  return (
+    <li>{note.content}</li>
+  )
+}
+
 const App = ({notes}) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map(note =>
-        <li key={note.id}>
-          {note.content}
-        </li>)}
+        <Note key={note.id} note={note} />
+        )}
       </ul>
     </div>
   )
