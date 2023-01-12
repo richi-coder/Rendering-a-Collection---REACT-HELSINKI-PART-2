f48584c5ad2e98b8f1bd4c5ce53ccfd2ad531bde
richi-coder <113008174+richi-coder@users.noreply.github.com>
2023-01-12 16:24:20 -0400

    OK

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,18 +1,42 @@
 const baseURL = "http://localhost:3000/notes";
 
-const readAll () => {
+const readAll = () => {
   fetch(baseURL)
       .then(response => response.json())
 }
 
-const create () => {
+const create = (newObject) => {
   fetch(baseURL, {
       method: "POST",
       headers: {
         "Accept": "application/json",
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(noteObject)
+      body: JSON.stringify(newObject)
     })
     .then(response => response.json())
+}
+
+const update = (id, newObject) => {
+  fetch(`${baseURL}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newObject)
+  })
+  .then(response => response.json())
+  .then(json => {
+    setNotes(notes.map(n => n.id !== id ? n : json))
+  })
+  .catch(error => {
+    setErrorMessage(
+      `Note '${note.content}' was already removed from server`
+    );
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000);
+    setNotes(notes.filter(n => n.id !== id))})
+}
 }
